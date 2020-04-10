53f4326eb320cb3b6e3ba2861bee1c2aa3800760
Leonardo Ruggieri <47697452+leonardoruggieri@users.noreply.github.com>
2020-04-10 19:20:59 +0200

    Update hpf_vi.py

--- hpf_vi.py
+++ hpf_vi.py
@@ -26,7 +26,7 @@ class hpf_vi():
         '''
         self.a, self.c, self.a1, self.b1, self.c1, self.d1, self.K = a, c, a1, b1, c1, d1, K
 
-    def fit(self, train, iterations):
+    def fit(self, train, iterations, valid=None):
         '''
         Fit the Hierarchical Poisson Factorization model via Coordinate Ascent Variational Algorithm (CAVI)
         and generates the corresponding observation matrix based on the variational parameters.
@@ -38,13 +38,23 @@ class hpf_vi():
             - iterations: int
               number of desired training epochs.
         '''
+        self.train = train
+        self.valid = valid
+
         # Dataset dimensions
         self.U, self.I = train.shape
-
+       
         self.initialize()
-        
+        self.mse_train = np.zeros(iterations)
+        if valid.shape[0] == train.shape[0]:
+            self.mse_valid = np.zeros(iterations)
+
+        def MSE(pred, values):
+            prediction, values = pred[values.nonzero()].flatten(), values[values.nonzero()].flatten()
+            return mean_squared_error(prediction, values)
 
         import time
+        from sklearn.metrics import mean_squared_error 
         tic = time.clock()
         for iter in range(iterations):
             for u, i in zip(train.nonzero()[0], train.nonzero()[1]):
@@ -62,25 +72,29 @@ class hpf_vi():
                 self.lambda_rte[i] = [self.tau_shp/self.tau_rte[i] + np.sum(self.gamma_shp[:,k]/self.gamma_rte[:,k]) for k in range(self.K)]
                 self.tau_rte[i] = self.c1/self.d1 + np.sum(self.lambda_shp[i]/self.lambda_rte[i])
 
-        # Building user preferences and item attributes
-        self.theta = self.gamma_shp/self.gamma_rte
-        self.beta = self.lambda_shp/self.lambda_rte
+            # Building user preferences and item attributes
+            self.theta = self.gamma_shp/self.gamma_rte
+            self.beta = self.lambda_shp/self.lambda_rte
 
-        # Generating observations y
-        self.predicted = np.dot(self.theta, self.beta.T)
+            # Generating observations y
+            self.predicted = np.dot(self.theta, self.beta.T)
+
+            #In-sample MSE
+            self.mse_train[iter] = MSE(self.predicted, self.train)
+
+            #Out-of-sample MSE
+            if valid.shape[0] == train.shape[0]:
+                self.mse_valid[iter] = MSE(self.predicted, self.valid)
+
+            
+
+
 
         toc = time.clock()
-        time = toc - tic
+        time = np.round(toc - tic,3)
         print(f"Completed in {time} seconds")
 
-    def mean_squared_error(self):
-        '''
-        Assess the convergence of the algorithm.
-        Please note that CAVI might find local optima. Moreover, this is only to qualitatively assess if our algorithm has somewhat reached a plausible result.
-        '''
-        from sklearn.metrics import mean_squared_error 
-        self.mse_error = mean_squared_error(train.flatten(), self.predicted.flatten())
-    
+
     def recommend(self, test, t = 0.3):
         '''
         Using the fitted algorithm to make recommendations to users of our dataset.
@@ -97,6 +111,7 @@ class hpf_vi():
                     recomm.append(i)
             if [i > 0 for i in recomm]:
                 print(f"User {u} may also like these items: {recomm}")
+
 
     def initialize(self):
         self.gamma_shp = np.random.uniform(0,1, size = (self.U, self.K)) + self.a
